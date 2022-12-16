4b4990611f516c25f93077d57780c4bbc6014cf5
ivorojas <ivo.2001@hotmail.com>
2022-12-16 00:05:27 -0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About () {
+    return (
+        <div>
+            <span>Esta página fue creada por Ivo Rojas</span>
+        </div>
+    )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Logo from '../img/logoHenry.png'
+import SearchBar from './SearchBar.jsx';
+import './Nav.css';
+import {Link} from "react-router-dom"
+import clima from '../img/clima-icon.png'
+
+function Nav({onSearch}) {
+  return (
+    <nav className="navbar_container">
+  
+    <div className="titulo_pagina">
+    <Link to='/'>
+      <div className='logo' >
+      <img id="logoHenry" src={clima} width="30" height="30" alt="" />
+      <h4> Weather App - Ivo Rojas</h4>
+      </div>
+      </Link>
+    
+    </div>
+
+
+    
+    <div className='searchbar' >
+    <SearchBar
+      onSearch={onSearch}
+    />
+    </div> 
+ 
+    </nav>
+  );
+};
+
+export default Nav;
